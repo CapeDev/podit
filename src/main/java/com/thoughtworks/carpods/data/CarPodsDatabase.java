6e9e0c0a6f11ee087c8da7df3e60619506026822
spargonaut <spargonaut@gmail.com>
2013-05-18 14:09:21 -0500

    JS - added method to insert basic pod info to database.

--- src/main/java/com/thoughtworks/carpods/data/CarPodsDatabase.java
+++ src/main/java/com/thoughtworks/carpods/data/CarPodsDatabase.java
@@ -227,6 +227,21 @@ public class CarPodsDatabase {
         return peopleList;
     }
 
+    public void savePod(Pod pod) {
+        String table = POD_TABLE;
+
+        ContentValues podInfo = new ContentValues();
+        podInfo.put(POD_NAME, pod.getPodName());
+        podInfo.put(POD_HOME_LOCATION, pod.getPodHomeLocation());
+        podInfo.put(POD_DEPARTURE_TIME, pod.getPodDepartureTime());
+        podInfo.put(POD_RETURN_TIME, pod.getPodReturnTime());
+        podInfo.put(ABOUT_POD, pod.getAboutPod());
+
+        this.open();
+        database.insert(table, null, podInfo);
+        this.close();
+    }
+
     private static class DBHelper extends SQLiteOpenHelper {
         private static final int DATABASE_VERSION = 1;
 
