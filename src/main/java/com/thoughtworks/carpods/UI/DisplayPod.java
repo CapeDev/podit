b18b6a167ae6b9571b8a1a2fb9273be8ef3d6deb
spargonaut <spargonaut@gmail.com>
2013-05-26 03:16:38 -0500

    JS - created the display pod activity

---
+++ src/main/java/com/thoughtworks/carpods/UI/DisplayPod.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.carpods.UI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.thoughtworks.carpods.R;
+
+
+public class DisplayPod extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.display_pod_view);
+
+
+    }
+}
