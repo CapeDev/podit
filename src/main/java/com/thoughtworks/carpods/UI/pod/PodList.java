6678dbc684fcccfa91a58c2f3875b5fd2776eac2
developer <developer@thoughtworks.com>
2013-07-13 13:46:13 -0500

    JS - implemented starting the EditPod Activity from an icon in the action bar on PodList. (cherry picked from commit 7ef51f3d)

--- src/main/java/com/thoughtworks/carpods/UI/pod/PodList.java
+++ src/main/java/com/thoughtworks/carpods/UI/pod/PodList.java
@@ -2,7 +2,10 @@ package com.thoughtworks.carpods.UI.pod;
 
 import android.app.ActionBar;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 import com.thoughtworks.carpods.R;
@@ -54,5 +57,17 @@ public class PodList extends ListPodActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_action_bar, menu);
+        return true;
+    }
+
+    public void add(MenuItem unused) {
+        Toast.makeText(this, "I just clicked the add button!", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this, EditPod.class));
     }
 }
